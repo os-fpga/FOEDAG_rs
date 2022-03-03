f5bd0c7f1e61a962f89df848c803fbc5e658a1d1
Alain Dargelas <alain.dargelas@gmail.com>
2022-03-02 18:07:05 -0800

    Initial customization of FOEDAG

---
+++ src/Main/main.cpp
@@ -0,0 +1,43 @@
+#include "Main/CommandLine.h"
+#include "Main/Foedag.h"
+#include "MainWindow/Session.h"
+#include "MainWindow/main_window.h"
+
+FOEDAG::Session* GlobalSession;
+
+QWidget* mainWindowBuilder(FOEDAG::CommandLine* cmd,
+                           FOEDAG::TclInterpreter* interp) {
+  FOEDAG::MainWindow* mainW = new FOEDAG::MainWindow{interp};
+  mainW->setWindowTitle("RAPTOR");
+  mainW->resize(800, 550);
+  return mainW;
+}
+
+void registerExampleCommands(QWidget* widget, FOEDAG::Session* session) {
+  auto hello = [](void* clientData, Tcl_Interp* interp, int argc,
+                  const char* argv[]) -> int {
+    GlobalSession->TclInterp()->evalCmd("puts Hello!");
+    return 0;
+  };
+  session->TclInterp()->registerCmd("hello", hello, 0, 0);
+}
+
+FOEDAG::GUI_TYPE getGuiType(const bool& withQt, const bool& withQml) {
+  if (!withQt) return FOEDAG::GUI_TYPE::GT_NONE;
+  if (withQml)
+    return FOEDAG::GUI_TYPE::GT_QML;
+  else
+    return FOEDAG::GUI_TYPE::GT_WIDGET;
+}
+
+int main(int argc, char** argv) {
+  FOEDAG::CommandLine* cmd = new FOEDAG::CommandLine(argc, argv);
+  cmd->processArgs();
+
+  FOEDAG::GUI_TYPE guiType = getGuiType(cmd->WithQt(), cmd->WithQml());
+
+  FOEDAG::Foedag* foedag =
+      new FOEDAG::Foedag(cmd, mainWindowBuilder, registerExampleCommands);
+
+  return foedag->init(guiType);
+}
