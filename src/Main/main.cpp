13827ee48dde85a9b0e569f8024ae5aaf42754e7
Volodymyr Kochyn <volodymyr.kochyn@rapidsilicon.com>
2022-03-04 19:08:03 +0200

    Fixed missing icons

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -31,6 +31,7 @@ FOEDAG::GUI_TYPE getGuiType(const bool& withQt, const bool& withQml) {
 }
 
 int main(int argc, char** argv) {
+  Q_INIT_RESOURCE(main_window_resource);
   FOEDAG::CommandLine* cmd = new FOEDAG::CommandLine(argc, argv);
   cmd->processArgs();
 
