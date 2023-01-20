fbffd0bb698be84d22b6724a1192b5246e02298a
Ravikiran Chollangi <ravikiran.chollangi@rapidsilicon.com>
2023-01-20 13:21:06 -0800

    GCC upgrade files

--- .github/workflows/install_ubuntu_dependencies_build.sh
+++ .github/workflows/install_ubuntu_dependencies_build.sh
@@ -1,7 +1,7 @@
 # Install required dependencies for Ubuntu systems
 sudo apt-get update -qq
 sudo apt install -y \
-  g++-9 \
+  g++-11 gcc-11 \
   tclsh \
   cmake \
   build-essential \
@@ -13,6 +13,6 @@ sudo apt install -y \
   xorg \
   qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools qtdeclarative5-dev \
   xvfb
-sudo ln -sf /usr/bin/g++-9 /usr/bin/g++
-sudo ln -sf /usr/bin/gcc-9 /usr/bin/gcc
-sudo ln -sf /usr/bin/gcov-9 /usr/bin/gcov
+sudo ln -sf /usr/bin/g++-11 /usr/bin/g++
+sudo ln -sf /usr/bin/gcc-11 /usr/bin/gcc
+sudo ln -sf /usr/bin/gcov-11 /usr/bin/gcov
