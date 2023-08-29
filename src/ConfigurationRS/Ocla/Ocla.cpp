7a365cfaa34c843245df7d32e872c49536de5cbf
Guan Yung <guan.yung@rapidsilicon.com>
2023-08-29 15:57:01 +0800

    Ocla initial skeleton stub code

---
+++ src/ConfigurationRS/Ocla/Ocla.cpp
@@ -0,0 +1,6 @@
+#include "Ocla.h"
+#include "CFGCommonRS/CFGArgRS_auto.h"
+#include "CFGObject/CFGObject_auto.h"
+
+void Ocla_entry(const CFGCommon_ARG* cmdarg) {
+}
---
+++ src/ConfigurationRS/Ocla/Test/Ocla_test.cpp
@@ -0,0 +1,6 @@
+#include "CFGCommonRS/CFGCommonRS.h"
+
+int main(int argc, const char** argv) {
+  CFG_POST_MSG("This is Ocla unit test");
+  return 0;
+}
