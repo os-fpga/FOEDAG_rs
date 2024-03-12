5a5e59644720453771805ee3fdd70f25ad77d121
NadeemYaseen <pu.engineerg@gmail.com>
2024-03-12 02:37:23 -0700

    change verilator bin path to share/verilator/bin

--- src/Main/main.cpp
+++ src/Main/main.cpp
@@ -91,7 +91,7 @@ int main(int argc, char** argv) {
     opcompiler->ProgrammerToolExecPath(openOcdPath);
     opcompiler->GetSimulator()->SetSimulatorPath(
         FOEDAG::Simulator::SimulatorType::Verilator,
-        (binpath / "HDL_simulator" / "verilator" / "bin").string());
+        (binpath / "HDL_simulator" / "verilator" / "share" / "verilator" / "bin").string());
     opcompiler->GetSimulator()->SetSimulatorPath(
         FOEDAG::Simulator::SimulatorType::GHDL,
         (binpath / "HDL_simulator" / "GHDL" / "bin").string());
