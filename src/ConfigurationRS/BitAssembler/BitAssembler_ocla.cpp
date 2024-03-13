6ad5bdbe4be85527b7dab9b2f78b6ae9a4b8f3e7
chungshien-chai <156862023+chungshien-chai@users.noreply.github.com>
2024-03-13 09:26:15 -0700

    Add EIO support (#672)

--- src/ConfigurationRS/BitAssembler/BitAssembler_ocla.cpp
+++ src/ConfigurationRS/BitAssembler/BitAssembler_ocla.cpp
@@ -12,6 +12,7 @@ void BitAssembler_OCLA::parse(CFGObject_BITOBJ& bitobj,
     std::ifstream file(analyzeCMDPath.c_str());
     CFG_ASSERT_MSG(file.is_open(), "Fail to open %s", analyzeCMDPath.c_str());
     std::string outPath = CFG_print("%s/ocla.ys", taskPath.c_str());
+    std::string out_report_Path = CFG_print("%s/ocla.ys.rpt", taskPath.c_str());
     CFG_POST_MSG("    OCLA Analyze CMD path: %s", outPath.c_str());
     std::ofstream outfile(outPath.c_str());
     CFG_ASSERT_MSG(outfile.is_open(), "Fail to open %s for writing",
@@ -26,7 +27,9 @@ void BitAssembler_OCLA::parse(CFGObject_BITOBJ& bitobj,
     file.close();
     outfile.close();
     // Execute yosys
-    std::string cmd = CFG_print("%s %s", yosysBin.c_str(), outPath.c_str());
+    std::string cmd =
+        CFG_print("cd %s && %s %s > %s", taskPath.c_str(), yosysBin.c_str(),
+                  outPath.c_str(), out_report_Path.c_str());
     std::string cmd_output = "";
     std::atomic<bool> stop = false;
     if (CFG_execute_cmd(cmd, cmd_output, nullptr, stop) == 0) {
@@ -51,6 +54,7 @@ void BitAssembler_OCLA::parse(CFGObject_BITOBJ& bitobj,
 void BitAssembler_OCLA::extract_ocla_info(CFGObject_BITOBJ& bitobj,
                                           nlohmann::json& json) {
   CFG_ASSERT(!bitobj.check_exist("ocla"));
+  nlohmann::json eio;
   nlohmann::json ocla;
   nlohmann::json ocla_debug_subsystem;
   std::string json_string = "";
@@ -62,6 +66,10 @@ void BitAssembler_OCLA::extract_ocla_info(CFGObject_BITOBJ& bitobj,
     CFG_POST_WARNING("Design does not contain messages");
     goto EXTRACT_OCLA_INFO_END;
   }
+  if (!json.contains("eio")) {
+    CFG_POST_WARNING("Design does not contain EIO information");
+    goto EXTRACT_OCLA_INFO_END;
+  }
   if (!json.contains("ocla")) {
     CFG_POST_WARNING("Design does not contain OCLA information");
     goto EXTRACT_OCLA_INFO_END;
@@ -71,21 +79,45 @@ void BitAssembler_OCLA::extract_ocla_info(CFGObject_BITOBJ& bitobj,
         "Design does not contain OCLA Debug Subsystem information");
     goto EXTRACT_OCLA_INFO_END;
   }
-  if (json.size() != 3) {
+  if (json.size() != 4) {
     CFG_POST_WARNING(
-        "JSON contains more that three expected entires: messages, OCLA, OCLA "
-        "Debug Subsystem");
+        "JSON contains more that four expected entires: messages, EIO, OCLA, "
+        "OCLA Debug Subsystem");
     goto EXTRACT_OCLA_INFO_END;
   }
+  eio = json["eio"];
   ocla = json["ocla"];
   ocla_debug_subsystem = json["ocla_debug_subsystem"];
+  if (!ocla_debug_subsystem.is_object()) {
+    CFG_POST_WARNING("JSON OCLA Debug Subsystem is not an object");
+    goto EXTRACT_OCLA_INFO_END;
+  }
+  if (!eio.is_object()) {
+    CFG_POST_WARNING("JSON EIO is not an object");
+    goto EXTRACT_OCLA_INFO_END;
+  }
   if (!ocla.is_array()) {
     CFG_POST_WARNING("JSON OCLA is not an array");
     goto EXTRACT_OCLA_INFO_END;
   }
-  if (!ocla_debug_subsystem.is_object()) {
-    CFG_POST_WARNING("JSON OCLA Debug Subsystem is not an object");
+  // Validate OCLA Debug Subsystem
+  if (!validate_ocla_debug_subsystem(ocla_debug_subsystem, eio)) {
+    CFG_POST_WARNING("Invalidate entire OCLA design");
     goto EXTRACT_OCLA_INFO_END;
+  }
+  // EIO
+  CFG_POST_MSG("    EIO Enabled: %d",
+               (uint32_t)(ocla_debug_subsystem["EIO_Enable"]));
+  if ((uint32_t)(ocla_debug_subsystem["EIO_Enable"])) {
+    CFG_POST_MSG("      Base Addr: 0x%08X", (uint32_t)(eio["addr"]));
+    CFG_POST_MSG("      probes_in: %d", eio["probes_in"].size());
+    for (auto& p : eio["probes_in"]) {
+      CFG_POST_MSG("        -> %s", std::string(p).c_str());
+    }
+    CFG_POST_MSG("      probes_out: %d", eio["probes_out"].size());
+    for (auto& p : eio["probes_out"]) {
+      CFG_POST_MSG("        -> %s", std::string(p).c_str());
+    }
   }
   // Loop through each
   CFG_POST_MSG("    Detected %d OCLA IP(s)", ocla.size());
@@ -94,10 +126,6 @@ void BitAssembler_OCLA::extract_ocla_info(CFGObject_BITOBJ& bitobj,
       CFG_POST_WARNING("Invalidate entire OCLA design");
       goto EXTRACT_OCLA_INFO_END;
     }
-  }
-  if (!validate_ocla_debug_subsystem(ocla_debug_subsystem)) {
-    CFG_POST_WARNING("Invalidate entire OCLA design");
-    goto EXTRACT_OCLA_INFO_END;
   }
   for (nlohmann::json& o : ocla) {
     CFG_POST_MSG("      OCLA IP:");
@@ -186,13 +214,81 @@ VALIDATE_OCLA_END:
   return status;
 }
 
-bool BitAssembler_OCLA::validate_ocla_debug_subsystem(
-    nlohmann::json& ocla_debug_subsystem) {
+bool BitAssembler_OCLA::validate_eio(nlohmann::json& eio) {
   bool status = false;
-  std::vector<std::string> params = {
-      "IP_VERSION",          "IP_ID",      "Cores",
-      "No_Probes",           "Probes_Sum", "No_AXI_Bus",
-      "AXI_Core_BaseAddress"};
+  const std::vector<std::string> params = {"Input_Probe_Width",
+                                           "Output_Probe_Width"};
+  if (!eio.is_object()) {
+    CFG_POST_WARNING("JSON EIO is not an object");
+    goto VALIDATE_EIO_END;
+  }
+  for (auto p : params) {
+    if (!eio.contains(p) ||
+        !(eio[p].is_number_integer() || eio[p].is_number_unsigned())) {
+      CFG_POST_WARNING(
+          "JSON EIO does not have parameter %s or the parameter type is "
+          "not number",
+          p.c_str());
+      goto VALIDATE_EIO_END;
+    }
+  }
+  if (!eio.contains("addr") ||
+      !(eio["addr"].is_number_integer() || eio["addr"].is_number_unsigned())) {
+    CFG_POST_WARNING(
+        "JSON EIO does not have addr information or the information type "
+        "is not number");
+    goto VALIDATE_EIO_END;
+  }
+  if (!eio.contains("probes_in") || !eio["probes_in"].is_array()) {
+    CFG_POST_WARNING(
+        "JSON EIO does not have probes_in information or the information "
+        "type is not an array");
+    goto VALIDATE_EIO_END;
+  }
+  if (!eio.contains("probes_out") || !eio["probes_out"].is_array()) {
+    CFG_POST_WARNING(
+        "JSON EIO does not have probes_out information or the information "
+        "type is not an array");
+    goto VALIDATE_EIO_END;
+  }
+  for (auto& probe : eio["probes_in"]) {
+    if (!probe.is_string()) {
+      CFG_POST_WARNING("JSON EIO probes_in signal is not a string");
+      goto VALIDATE_EIO_END;
+    }
+  }
+  for (auto& probe : eio["probes_out"]) {
+    if (!probe.is_string()) {
+      CFG_POST_WARNING("JSON EIO probes_out signal is not a string");
+      goto VALIDATE_EIO_END;
+    }
+  }
+  // Type param, params, addr (not param), probes_in, probes_out (not param)
+  if (eio.size() != (1 + params.size() + 2)) {
+    CFG_POST_WARNING(
+        "JSON EIO has extra object key. Expected %d count, found %d",
+        1 + params.size() + 2, eio.size());
+    goto VALIDATE_EIO_END;
+  }
+  status = true;
+VALIDATE_EIO_END:
+  return status;
+}
+
+bool BitAssembler_OCLA::validate_ocla_debug_subsystem(
+    nlohmann::json& ocla_debug_subsystem, nlohmann::json& eio) {
+  bool status = false;
+  std::vector<std::string> params = {"AXI_Core_BaseAddress",
+                                     "Cores",
+                                     "EIO_BaseAddress",
+                                     "EIO_Enable",
+                                     "Input_Probe_Width",
+                                     "IP_ID",
+                                     "IP_VERSION",
+                                     "No_AXI_Bus",
+                                     "No_Probes",
+                                     "Output_Probe_Width",
+                                     "Probes_Sum"};
   std::vector<std::string> str_params = {"Mode", "Axi_Type", "Sampling_Clk"};
   for (uint32_t i = 0; i < 15; i++) {
     params.push_back(CFG_print("Probe%02d_Width", i + 1));
@@ -241,6 +337,18 @@ bool BitAssembler_OCLA::validate_ocla_debug_subsystem(
         "found %d",
         1 + params.size() + str_params.size(), ocla_debug_subsystem.size());
     goto VALIDATE_OCLA_DEBUG_SUBSYSTEM_END;
+  }
+  // Validate EIO
+  if ((uint32_t)(ocla_debug_subsystem["EIO_Enable"])) {
+    if (!validate_eio(eio)) {
+      CFG_POST_WARNING("EIO is invalid");
+      goto VALIDATE_OCLA_DEBUG_SUBSYSTEM_END;
+    }
+  } else {
+    if (eio.size()) {
+      CFG_POST_WARNING("EIO is not enabled but EIO object is not empty");
+      goto VALIDATE_OCLA_DEBUG_SUBSYSTEM_END;
+    }
   }
   status = true;
 VALIDATE_OCLA_DEBUG_SUBSYSTEM_END:
